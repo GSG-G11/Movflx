08894c1799eb1c6c0f731b27bf44a5f5a8d2290f
karam-zomlut <zomlutk@gmail.com>
2022-04-19 23:00:18 +0300

    Fetch Data in Top Rated Section

--- src/Components/TopMovies/index.jsx
+++ src/Components/TopMovies/index.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
-const TopMovies = () => {
+const TopMovies = ({ filterCtg, setFilterCtg, topMovies }) => {
+  const handleFilterCtg = (e) => {
+    setFilterCtg(e.target.textContent);
+  };
+
   return (
-    <div>TopMovies</div>
-  )
-}
+    <section className='new-sec top-rated-sec' id='Movies'>
+      <div className='container'>
+        <div className='section-title'>
+          <h5 className='sub-title'>ONLINE STREAMING</h5>
+          <h2 className='title'>Top Rated Movies</h2>
+        </div>
+        <div className='btns-div categories-btns'>
+          <button
+            className={
+              filterCtg === 'Action'
+                ? 'btn category-btn active'
+                : 'btn category-btn'
+            }
+            onClick={(e) => handleFilterCtg(e)}>
+            Action
+          </button>
+          <button
+            className={
+              filterCtg === 'Comedy'
+                ? 'btn category-btn active'
+                : 'btn category-btn'
+            }
+            onClick={(e) => handleFilterCtg(e)}>
+            Comedy
+          </button>
+          <button
+            className={
+              filterCtg === 'Romance'
+                ? 'btn category-btn active'
+                : 'btn category-btn'
+            }
+            onClick={(e) => handleFilterCtg(e)}>
+            Romance
+          </button>
+          <button
+            className={
+              filterCtg === 'Horror'
+                ? 'btn category-btn active'
+                : 'btn category-btn'
+            }
+            onClick={(e) => handleFilterCtg(e)}>
+            Horror
+          </button>
+        </div>
+        <div className='row'>
+          {
+          topMovies
+          ?
+          topMovies.map(movie => (
+            <h1>{movie.Title}</h1>
+          ))
+          :
+          null
+          }
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default TopMovies;
