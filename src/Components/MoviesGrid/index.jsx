484a87d3ff3fd90bd09ef00800800a8c900f6c16
karam-zomlut <zomlutk@gmail.com>
2022-04-19 17:18:07 +0300

    Finish App Components Relates #3

---
+++ src/Components/MoviesGrid/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MoviesGrid = () => {
+  return (
+    <div>MoviesGrid</div>
+  )
+}
+
+export default MoviesGrid;
---
+++ src/Components/SubBanner/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SubBanner = () => {
+  return (
+    <div>SubBanner</div>
+  )
+}
+
+export default SubBanner;
