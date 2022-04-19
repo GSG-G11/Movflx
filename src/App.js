00d09a2da759438705bdfaeead7ed494d2af0733
karam-zomlut <zomlutk@gmail.com>
2022-04-19 17:34:25 +0300

    Setup Routes in App.js Relates #3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import SearchPage from "./Pages/SearchPage";
+import SingleMovie from "./Pages/SingleMovie";
 
 function App() {
   return (
     <div className="App">
-      <h1>Movflx Test</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/movie/:id" element={<SingleMovie />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
