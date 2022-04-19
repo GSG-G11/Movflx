5b9ff6a44ae74aa281c1d9f8171753a8e6a7c51d
karam-zomlut <zomlutk@gmail.com>
2022-04-20 02:38:56 +0300

    Finsih Search Function Relates #14

--- src/Components/Search/index.jsx
+++ src/Components/Search/index.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './style.css';
 
-const Search = () => {
+const Search = ({ showSearch, setShowSearch }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const searchPage = useNavigate();
+
+  const sendSearchQuery = (e) => {
+    e.preventDefault();
+    if (searchQuery === '') {
+      alert('Please enter a search query');
+    }
+    else {
+      searchPage(`/search/${searchQuery}`);
+      setShowSearch(false);
+      setSearchQuery('');
+    }
+  };
+
+  const hideShowSearch = (e) => {
+    e.preventDefault();
+    if(e.target.className === 'search-container show') {
+      setShowSearch(prev => false);
+    }
+    return;
+  }
+
+  useEffect(() => {
+    if(showSearch) {
+      document.body.style = 'overflow: hidden';
+    }
+
+    return () => {
+      document.body.style = 'overflow: auto';
+    }
+  }, [showSearch]);
+
   return (
-    <div>Search</div>
-  )
-}
+    <div className={showSearch ? 'search-container show' : 'search-container'} onClick={(e) => hideShowSearch(e)}>
+      <form>
+        <input
+          type='text'
+          placeholder='Search'
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          autoFocus
+        />
+        <button className='link btn search-btn' onClick={(e) => sendSearchQuery(e)}>
+          <i className='ri-search-line'></i>
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Search;
