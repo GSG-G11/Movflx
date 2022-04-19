0c8699b273483e075d4e7544d27b0baee8212e48
karam-zomlut <zomlutk@gmail.com>
2022-04-20 01:18:46 +0300

    Finish Details Page

--- src/Pages/SingleMovie.jsx
+++ src/Pages/SingleMovie.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import MovieDetails from '../Components/MovieDetails'
 
 const SingleMovie = () => {
   return (
-    <div>SingleMovie</div>
+    <MovieDetails  />
   )
 }
 
