5fc49a41c8bf5184a7c11deae5630ba942ca05c8
karam-zomlut <zomlutk@gmail.com>
2022-04-20 11:32:17 +0300

    Finish Subscribe Section Relates #16

--- src/Components/Subscribe/index.jsx
+++ src/Components/Subscribe/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="subscribe-sec">
+      <div className="container">
+        <div className="row">
+          <div className="subscribe-content">
+            <h3 className='title'>Trial Start First 30 Days.</h3>
+            <p className="desc">Enter your email to create or restart your membership.</p>
+          </div>
+          <div className="subscribe-form">
+            <form className='form'>
+              <input type="email" placeholder="Enter your email" />
+              <button type="submit">Get Started</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
